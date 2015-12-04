0c43c9bc3bf463e024bed88b4a3eec9a02dd637f
kelly <kelly@shareablee.com>
2015-12-04 14:35:59 -0500

    rendering with templates now

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def create_():
-    return 'Hello World!'
+def create_form():
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run()
